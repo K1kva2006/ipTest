bb247cbfaa7ca30ed526084bada00653b89a2a67
K1kva2006 <115255875+K1kva2006@users.noreply.github.com>
2024-09-12 14:13:13 +0400

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
-    console.log(req.headers["x-forwarded-for"] || req.connection.remoteAddress)
-    res.status(200).json(req.headers["x-forwarded-for"] || req.connection.remoteAddress)
+    console.log(req.headers["x-forwarded-for"])
+    res.status(200).json(req.headers["x-forwarded-for"])
 });
 
 app.listen(3000, () => console.log("server is running"));
